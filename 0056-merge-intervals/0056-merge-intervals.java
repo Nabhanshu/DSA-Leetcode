6cc0c231384d1701c181af032da8bfa9c79c7e41
Nabhanshu Sharma <119276180+Nabhanshu@users.noreply.github.com>
2024-07-04 23:26:17 +0530

    Time: 8 ms (84.62%), Space: 46 MB (92.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,52 +1,50 @@
 class Solution {
     public int[][] merge(int[][] arr) {
 
-    //     int ans[][]=new int[arr.length][arr[0].length];
+        // int ans[][]=new int[arr.length][arr[0].length];
 
-    //     Arrays.sort(arr,new Comparator<int []> (){
-    //         public int compare(int a[],int b[]){
-    //             return a[0]-b[0];
-    //         }
-    //     });
+        // Arrays.sort(arr,new Comparator<int []> (){
+        //     public int compare(int a[],int b[]){
+        //         return a[0]-b[0];
+        //     }
+        // });
 
-    //     for(int i=0;i<arr.length;i++){
-    //             if(ans.length==0 || ans[ans.length-1][1]<arr[i][0]){
+        // for(int i=0;i<arr.length;i++){
+        //         if(ans.length==0 || ans[ans.length-1][1]<arr[i][0]){
 
-    //                 ans[i][0]=arr[i][0];
-    //                 ans[i][1]=arr[i][1];
+        //             ans[i][0]=arr[i][0];
+        //             ans[i][1]=arr[i][1];
 
-    //             }else{
-    //                 ans[ans.length-1][1]=Math.max(ans[ans.length-1][1],arr[i][1]);
-    //             }
+        //         }else{
+                    
+        //             ans[ans.length-1][1]=Math.max(ans[ans.length-1][1],arr[i][1]);
+        //         }
 
-    //     }
+        // }
 
-    //     return ans;
+        // return ans;
 
-    //     int n = arr.length; // size of the array
-    //     //sort the given intervals:
-    //     Arrays.sort(arr, new Comparator<int[]>() {
-    //         public int compare(int[] a, int[] b) {
-    //             return a[0] - b[0];
-    //         }
-    //     });
+//         int n = arr.length; // size of the array
+//         //sort the given intervals:
+//         Arrays.sort(arr, new Comparator<int[]>() {
+//             public int compare(int[] a, int[] b) {
+//                 return a[0] - b[0];
+//             }
+//         });
+// List<List<Integer>> ans = new ArrayList<>();
 
-    //       List<int []> ans = new ArrayList<>();
-
-    //     for (int i = 0; i < n; i++) {
-    //         // if the current interval does not
-    //         // lie in the last interval:
-    //         if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
-    //             ans.add(arr[i][0], arr[i][1]);
-    //         }
-    //         // if the current interval
-    //         // lies in the last interval:
-    //         else {
-    //             ans.get(ans.size() - 1).set(1,
-    //                                         Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
-    //         }
-    //     }
-    //   return  ans.toArray(new int[ans.size()][]);
+//         for (int i = 0; i < n; i++) {
+          
+//             if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+//              ans.add(Arrays.asList(arr[i][0], arr[i][1]));
+//             }
+          
+//             else {
+//                 ans.get(ans.size() - 1).set(1,
+//                                             Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
+//             }
+//         }
+//       return  ans.toArray(new int[ans.size()][]);
 
     if (arr.length <= 1) {
             return arr;
