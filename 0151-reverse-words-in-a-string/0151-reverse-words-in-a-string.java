7c1c251581f8c616b0b5a4958f8474e332c3d2f8
Nabhanshu Sharma <119276180+Nabhanshu@users.noreply.github.com>
2024-07-18 15:49:00 +0530

    Time: 10 ms (39.68%), Space: 45.1 MB (25.96%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,66 @@
+class Solution {
+    public String reverseWords(String s) {
+    // int left = 0;
+	// int right = s.length() - 1;
+
+	// String temp = "";
+	// String ans = "";
+
+	// //Iterate the string and keep on adding to form a word
+	// //If empty space is encountered then add the current word to the result
+	// while (left <= right)
+	// {
+	// 	char ch = s.charAt(left);
+	// 	if (ch != ' ')
+	// 	{
+	// 		temp += ch;
+	// 	}
+	// 	else if (ch == ' ')
+	// 	{
+	// 		if (!ans.equals(""))
+	// 		{
+	// 			ans = temp + " " + ans;
+	// 		}
+	// 		else
+	// 		{
+	// 			ans = temp;
+	// 		}
+	// 		temp = "";
+	// 	}
+	// 	left++;
+	// }
+
+	// //If not empty string then add to the result(Last word is added)
+	// if (!temp.equals(""))
+	// {
+	// 	if (!ans.equals(""))
+	// 	{
+	// 		ans = temp + " " + ans;
+	// 	}
+	// 	else
+	// 	{
+	// 		ans = temp;
+	// 	}
+	// }
+
+	// return ans;
+
+    // optimal 
+    String result = new String();
+    int i = 0;
+    int n = s.length();
+
+    while(i < n){
+        while(i < n && s.charAt(i) == ' ') i++;
+        if(i >= n) break;
+        int j = i + 1;
+        while(j < n && s.charAt(j) != ' ') j++;
+        String sub = s.substring(i, j);
+        if(result.length() == 0) result = sub;
+        else result = sub + " " + result;
+        i = j+1;
+    }
+    return result;
+
+    }
+}
