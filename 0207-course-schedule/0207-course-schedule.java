3e8efb31583d9f078248223bf8ddb01908e0382a
Nabhanshu Sharma <119276180+Nabhanshu@users.noreply.github.com>
2024-07-30 21:20:21 +0530

    Time: 5 ms (66.81%), Space: 44.3 MB (73.83%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,41 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-         if (numCourses <= 1 || prerequisites == null || prerequisites.length <= 1) {
-            return true;
-        }
-        int[][] matrix = new int[numCourses][numCourses];
-        int[] requirements = new int[numCourses];
+    public boolean canFinish(int n, int[][] prerequisites) {
+          List<Integer>[] adj = new List[n];
+        int[] indegree = new int[n];
+        List<Integer> ans = new ArrayList<>();
 
-        for (int[] each : prerequisites) {
-            int course = each[0];
-            int pre = each[1];
-            if (matrix[pre][course] == 0) {
-                requirements[course]++;
+        for (int[] pair : prerequisites) {
+            int course = pair[0];
+            int prerequisite = pair[1];
+            if (adj[prerequisite] == null) {
+                adj[prerequisite] = new ArrayList<>();
             }
-            matrix[pre][course] = 1;
+            adj[prerequisite].add(course);
+            indegree[course]++;
         }
-        java.util.Queue<Integer> stack = new java.util.LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (requirements[i] == 0) {
-                stack.add(i);
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        int counter = 0;
-        while (!stack.isEmpty()) {
-            int course = stack.poll();
-            counter++;
-            for (int i = 0; i < numCourses; i++) {
-                if (matrix[course][i] != 0) {
-                    requirements[i]--;
-                    if (requirements[i] == 0) {
-                        stack.add(i);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            ans.add(current);
+
+            if (adj[current] != null) {
+                for (int next : adj[current]) {
+                    indegree[next]--;
+                    if (indegree[next] == 0) {
+                        queue.offer(next);
                     }
                 }
             }
         }
-        return counter == numCourses;
+
+        return ans.size() == n;
+        
     }
 }
