8b98bdfc13ea0a4502b224a95912381ef18c38b7
Nabhanshu Sharma <119276180+Nabhanshu@users.noreply.github.com>
2024-10-09 21:31:33 +0530

    Time: 0 ms (100%), Space: 41.7 MB (49.87%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
+            
            
             if (digits[i] < 9) {
                 digits[i]++;
@@ -10,7 +11,6 @@ class Solution {
             digits[i] = 0;
         }
         
-       
         int[] ans= new int[digits.length + 1];
         ans[0] = 1; 
         return ans;
