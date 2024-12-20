dfe570f7ee8a024b4832cd62c44a531287aaeae1
Nabhanshu Sharma <119276180+Nabhanshu@users.noreply.github.com>
2024-12-20 20:51:21 +0530

    Time: 2 ms (98.9%), Space: 45.5 MB (5.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,9 +15,48 @@ class Solution {
 
         // Hashing
 
+        // int ans[]=new int[2];
+        // HashMap<Integer,Integer> hm= new HashMap<>();
+        // for(int i=0;i<arr.length;i++){
+        //     int req=target-arr[i];
+        //     if(hm.containsKey(req)){
+        //         ans[0]=hm.get(req);
+        //         ans[1]=i;
+        //         break;
+        //     }else{
+        //         hm.put(arr[i],i);
+        //     }
+        // }
+
+        // return ans;
+
+        // 2 pointer
+
+        // Arrays.sort(arr);
+   
+        // int ans[]=new int[2];
+        // int n=arr.length;
+        // int left=0;
+        // int right=n-1;
+        // while(left<right){
+        //     int sum=arr[left]+arr[right];
+        // if(sum==target){
+        //     ans[0]=left;
+        //     ans[1]=right;
+        //      break;
+        // }else if(sum<target){
+        //     left++;
+        // }else{
+        //     right--;
+        // }
+        // }
+
+        // return ans;
+
+        HashMap<Integer,Integer> hm =new HashMap<>();
         int ans[]=new int[2];
-        HashMap<Integer,Integer> hm= new HashMap<>();
-        for(int i=0;i<arr.length;i++){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
             int req=target-arr[i];
             if(hm.containsKey(req)){
                 ans[0]=hm.get(req);
@@ -26,9 +65,11 @@ class Solution {
             }else{
                 hm.put(arr[i],i);
             }
-        }
 
+        }
         return ans;
+
+
         
     }
 }
